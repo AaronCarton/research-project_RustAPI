bbde51bd16854514a398ceaee574166a366dc80b
Aaron <aaron.carton@student.howest.be>
2023-01-29 20:39:10 +0100

    feat: add admin guard to create_quiz route

--- src/routes/quiz_routes.rs
+++ src/routes/quiz_routes.rs
@@ -1,3 +1,4 @@
+use crate::guards::adminguard::AdminGuard;
 use crate::guards::authguard::AuthGuard;
 use crate::models::clientresponse::ClientResponse;
 use crate::models::question::AnswerQuestionModel;
@@ -6,7 +7,7 @@ use crate::services::{quiz_service, user_service};
 use rocket::serde::json::Json;
 
 #[post("/", data = "<payload>")]
-pub fn create_quiz(payload: Json<NewQuiz>) -> Json<Quiz> {
+pub fn create_quiz(_admin: AdminGuard, payload: Json<NewQuiz>) -> Json<Quiz> {
     let quiz = payload.into_inner();
     Json(quiz_service::create_quiz(quiz))
 }
